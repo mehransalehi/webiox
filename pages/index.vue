eec5649f68f1897dcc8ca420069b10cbba57a679
Mehran <alonephoenix90@gmail.com>
2024-12-22 16:56:55 +0330

    add sliders

--- pages/index.vue
+++ pages/index.vue
@@ -7,107 +7,139 @@
         </div>
       </div>
       <!-- slider section -->
-      <section class="slider_section">
-        <div id="customCarousel1" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="detail-box">
-                      <h1>
-                        Crypto <br />
-                        Currency
-                      </h1>
-                      <p>
-                        Explicabo esse amet tempora quibusdam laudantium,
-                        laborum eaque magnam fugiat hic? Esse dicta aliquid
-                        error repudiandae earum suscipit fugiat molestias,
-                        veniam, vel architecto veritatis delectus repellat modi
-                        impedit sequi.
-                      </p>
-                      <div class="btn-box">
-                        <a href="" class="btn1"> Read More </a>
-                      </div>
+      <section class="slider_section text-primary">
+        <swiper ::spaceBetween="30" :centeredSlides="true" :autoplay="{
+          delay: 8500,
+          disableOnInteraction: false,
+        }" :pagination="{
+          clickable: true,
+        }" :navigation="true" :modules="[Autoplay, Pagination, Navigation]" @autoplayTimeLeft="onAutoplayTimeLeft"
+          class="mySwiper">
+          <swiper-slide>
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-6 ">
+                  <div class="detail-box">
+                    <h1>
+                      Crypto <br>
+                      Currency
+                    </h1>
+                    <p>
+                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta
+                      aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis
+                      delectus repellat modi impedit sequi.
+                    </p>
+                    <div class="btn-box">
+                      <a href="" class="btn1">
+                        Read More
+                      </a>
                     </div>
                   </div>
-                  <div class="col-md-6">
-                    <div class="img-box">
-                      <img src="/images/slider-img.png" alt="" />
-                    </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="img-box">
+                    <img src="/images/slider-img.png" alt="">
                   </div>
                 </div>
               </div>
             </div>
-            <div class="carousel-item">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="detail-box">
-                      <h1>
-                        Crypto <br />
-                        Currency
-                      </h1>
-                      <p>
-                        Explicabo esse amet tempora quibusdam laudantium,
-                        laborum eaque magnam fugiat hic? Esse dicta aliquid
-                        error repudiandae earum suscipit fugiat molestias,
-                        veniam, vel architecto veritatis delectus repellat modi
-                        impedit sequi.
-                      </p>
-                      <div class="btn-box">
-                        <a href="" class="btn1"> Read More </a>
-                      </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-6 ">
+                  <div class="detail-box">
+                    <h1>
+                      Crypto <br>
+                      Currency
+                    </h1>
+                    <p>
+                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta
+                      aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis
+                      delectus repellat modi impedit sequi.
+                    </p>
+                    <div class="btn-box">
+                      <a href="" class="btn1">
+                        Read More
+                      </a>
                     </div>
                   </div>
-                  <div class="col-md-6">
-                    <div class="img-box">
-                      <img src="/images/slider-img.png" alt="" />
-                    </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="img-box">
+                    <img src="/images/slider-img.png" alt="">
                   </div>
                 </div>
               </div>
             </div>
-            <div class="carousel-item">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="detail-box">
-                      <h1>
-                        Crypto <br />
-                        Currency
-                      </h1>
-                      <p>
-                        Explicabo esse amet tempora quibusdam laudantium,
-                        laborum eaque magnam fugiat hic? Esse dicta aliquid
-                        error repudiandae earum suscipit fugiat molestias,
-                        veniam, vel architecto veritatis delectus repellat modi
-                        impedit sequi.
-                      </p>
-                      <div class="btn-box">
-                        <a href="" class="btn1"> Read More </a>
-                      </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-6 ">
+                  <div class="detail-box">
+                    <h1>
+                      Crypto <br>
+                      Currency
+                    </h1>
+                    <p>
+                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta
+                      aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis
+                      delectus repellat modi impedit sequi.
+                    </p>
+                    <div class="btn-box">
+                      <a href="" class="btn1">
+                        Read More
+                      </a>
                     </div>
                   </div>
-                  <div class="col-md-6">
-                    <div class="img-box">
-                      <img src="/images/slider-img.png" alt="" />
-                    </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="img-box">
+                    <img src="/images/slider-img.png" alt="">
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <ol class="carousel-indicators">
-            <li
-              data-target="#customCarousel1"
-              data-slide-to="0"
-              class="active"
-            ></li>
-            <li data-target="#customCarousel1" data-slide-to="1"></li>
-            <li data-target="#customCarousel1" data-slide-to="2"></li>
-          </ol>
-        </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-6 ">
+                  <div class="detail-box">
+                    <h1>
+                      Crypto <br>
+                      Currency
+                    </h1>
+                    <p>
+                      Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta
+                      aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis
+                      delectus repellat modi impedit sequi.
+                    </p>
+                    <div class="btn-box">
+                      <a href="" class="btn1">
+                        Read More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="img-box">
+                    <img src="/images/slider-img.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </swiper-slide>
+          <template #container-end>
+            <div class="autoplay-progress">
+              <svg viewBox="0 0 48 48" ref="progressCircle">
+                <circle cx="24" cy="24" r="20"></circle>
+              </svg>
+              <span ref="progressContent"></span>
+            </div>
+          </template>
+        </swiper>
       </section>
       <!-- end slider section -->
     </div>
@@ -407,98 +439,152 @@
         <div class="heading_container heading_center psudo_white_primary mb_45">
           <h2>What says our <span>Customers</span></h2>
         </div>
-        <div class="carousel-wrap">
-          <div class="owl-carousel client_owl-carousel">
-            <div class="item">
-              <div class="box">
-                <div class="img-box">
-                  <img src="/images/client1.jpg" alt="" class="box-img" />
-                </div>
-                <div class="detail-box">
-                  <div class="client_id">
-                    <div class="client_info">
-                      <h6>LusDen</h6>
-                      <p>magna aliqua. Ut</p>
-                    </div>
-                    <i class="fa fa-quote-left" aria-hidden="true"></i>
+        <swiper ref="{swiperRef}" :slidesPerView="2" :centeredSlides="true" :spaceBetween="30" :pagination="{
+          type: 'fraction',
+        }" :navigation="true" :modules="[Pagination, Navigation]" class="mySwiper pb-5">
+          <swiper-slide>
+            <div class="box">
+              <div class="img-box">
+                <img src="/images/client1.jpg" alt="" class="box-img" />
+              </div>
+              <div class="detail-box">
+                <div class="client_id">
+                  <div class="client_info">
+                    <h6>LusDen</h6>
+                    <p>magna aliqua. Ut</p>
                   </div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis
-                  </p>
+                  <i class="fa fa-quote-left" aria-hidden="true"></i>
                 </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis
+                </p>
               </div>
             </div>
-            <div class="item">
-              <div class="box">
-                <div class="img-box">
-                  <img src="/images/client2.jpg" alt="" class="box-img" />
-                </div>
-                <div class="detail-box">
-                  <div class="client_id">
-                    <div class="client_info">
-                      <h6>Zen Court</h6>
-                      <p>magna aliqua. Ut</p>
-                    </div>
-                    <i class="fa fa-quote-left" aria-hidden="true"></i>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="box">
+              <div class="img-box">
+                <img src="/images/client1.jpg" alt="" class="box-img" />
+              </div>
+              <div class="detail-box">
+                <div class="client_id">
+                  <div class="client_info">
+                    <h6>LusDen</h6>
+                    <p>magna aliqua. Ut</p>
                   </div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis
-                  </p>
+                  <i class="fa fa-quote-left" aria-hidden="true"></i>
                 </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis
+                </p>
               </div>
             </div>
-            <div class="item">
-              <div class="box">
-                <div class="img-box">
-                  <img src="/images/client1.jpg" alt="" class="box-img" />
-                </div>
-                <div class="detail-box">
-                  <div class="client_id">
-                    <div class="client_info">
-                      <h6>LusDen</h6>
-                      <p>magna aliqua. Ut</p>
-                    </div>
-                    <i class="fa fa-quote-left" aria-hidden="true"></i>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="box">
+              <div class="img-box">
+                <img src="/images/client1.jpg" alt="" class="box-img" />
+              </div>
+              <div class="detail-box">
+                <div class="client_id">
+                  <div class="client_info">
+                    <h6>LusDen</h6>
+                    <p>magna aliqua. Ut</p>
                   </div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis
-                  </p>
+                  <i class="fa fa-quote-left" aria-hidden="true"></i>
                 </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis
+                </p>
               </div>
             </div>
-            <div class="item">
-              <div class="box">
-                <div class="img-box">
-                  <img src="/images/client2.jpg" alt="" class="box-img" />
-                </div>
-                <div class="detail-box">
-                  <div class="client_id">
-                    <div class="client_info">
-                      <h6>Zen Court</h6>
-                      <p>magna aliqua. Ut</p>
-                    </div>
-                    <i class="fa fa-quote-left" aria-hidden="true"></i>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="box">
+              <div class="img-box">
+                <img src="/images/client1.jpg" alt="" class="box-img" />
+              </div>
+              <div class="detail-box">
+                <div class="client_id">
+                  <div class="client_info">
+                    <h6>LusDen</h6>
+                    <p>magna aliqua. Ut</p>
                   </div>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis
-                  </p>
+                  <i class="fa fa-quote-left" aria-hidden="true"></i>
                 </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis
+                </p>
               </div>
             </div>
-          </div>
-        </div>
+          </swiper-slide>
+        </swiper>
       </div>
     </section>
 
     <!-- end client section -->
   </div>
 </template>
-  
+
+
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+
+const progressCircle = ref(null);
+const progressContent = ref(null);
+const onAutoplayTimeLeft = (s, time, progress) => {
+  progressCircle.value.style.setProperty('--progress', 1 - progress);
+  progressContent.value.textContent = `${Math.ceil(time / 1000)}s`;
+};
+
+</script>
+
+
+<style>
+.autoplay-progress {
+  position: absolute;
+  right: 16px;
+  bottom: 16px;
+  z-index: 10;
+  width: 48px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  color: var(--swiper-theme-color);
+}
+
+.autoplay-progress svg {
+  --progress: 0;
+  position: absolute;
+  left: 0;
+  top: 0px;
+  z-index: 10;
+  width: 100%;
+  height: 100%;
+  stroke-width: 4px;
+  stroke: var(--swiper-theme-color);
+  fill: none;
+  stroke-dashoffset: calc(125.6px * (1 - var(--progress)));
+  stroke-dasharray: 125.6;
+  transform: rotate(-90deg);
+}
+</style>
